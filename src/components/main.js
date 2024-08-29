4739186ce89e3b05c707df96e076fa17f4b9686f
Tomilade18 <jboyyumzey@gmail.com>
2024-08-29 16:25:01 +0100

    setting layout

--- src/components/main.js
+++ src/components/main.js
@@ -3,16 +3,22 @@ import './main.css'
 
 export default function Landing () {
     return (
-        <div className='header'>
-            <div className='left-hand-side'>
-            <img src="./events.png" className="suc-img" alt="" />
-            </div>
-            <div className='right-hand-side'>
-                <p>Schedule</p>
-                <p>Ticket</p>
-                <p>Contact</p>
-                <button>Login</button>
+      <div className='page-container'>
+          <div className='top-section'>
+            <div className='header'>
+                <div className='left-hand-side'>
+                <img src="./events.png" className="suc-img" alt="" />
+                </div>
+                <div className='right-hand-side'>
+                    <p>Schedule</p>
+                    <p>Ticket</p>
+                    <p>Contact</p>
+                    <button>Login</button>
+                </div>
             </div>
         </div>
+        <div className='middle-section'></div>
+        <div className='bottom-section'></div>
+      </div>
     )
 }

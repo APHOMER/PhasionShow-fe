26a4061c666e4ea355484b5fd2b4c788cde4b97d
Tunwase Ayobami <tunwaseayobami98@gmail.com>
2024-09-04 00:57:29 +0100

    Started styling login page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -20,9 +20,25 @@ const Register = ({ setAuthToken }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" required />
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+        required
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email" required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password" required
+      />
       <button type="submit">Register</button>
     </form>
   );

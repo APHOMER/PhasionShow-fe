f376745ec8eb86fd2ce283bf081ca6169a5c1afe
Aphomer <aaphomer@gmail.com>
2024-12-01 13:44:32 +0100

    returned html form tag

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -99,7 +99,7 @@ const Register = ({ setAuthToken }) => {
                     />
                   </div>
                   <div className="input-wrapper">
-                    {/* <label htmlFor="date-of-birth">Date of birth <span className="date-format">(MM/DD/YY)</span></label> */}
+                    <label htmlFor="date-of-birth">Date of birth <span className="date-format">(MM/DD/YY)</span></label>
                     <input
                       // id="date-of-birth"
                       // type="date"

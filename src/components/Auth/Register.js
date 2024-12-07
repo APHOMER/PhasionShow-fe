65deb438aa0a4b86104a79a82e7a130fa4dd0c21
Aphomer <aaphomer@gmail.com>
2024-12-07 18:34:27 +0100

    returned Date of Birth for testing

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -13,7 +13,7 @@ const Register = ({ setAuthToken }) => {
     "firstName": "",
     "lastName": "",
     "email": "",
-    // "dateOfBirth": "",
+    "dateOfBirth": "",
     "password": "",
     "confirmPassword": ""
   });
